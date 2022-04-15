6031a08debdf94376a3f8a8b865aa3ca54ba7aac
Ethan Lin <lyc.ethan@gmail.com>
2022-04-16 01:17:34 +0800

    Get link url form onOpenURL

--- DeepLink/DeepLink/DeepLinkApp.swift
+++ DeepLink/DeepLink/DeepLinkApp.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 @main
 struct DeepLinkApp: App {
+    
+    @StateObject var homeViewModel: HomeViewModel = HomeViewModel()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(homeViewModel)
+                .onOpenURL { url in
+                    if homeViewModel.checkDeepLink(url: url) {
+                        print("From Deep Link")
+                    } else {
+                        print("Fail back Deep Link")
+                    }
+                }
         }
     }
+    
 }
