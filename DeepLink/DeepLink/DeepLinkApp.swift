43ca4ff276c86166f8c9f5264090d29d94e99a0b
Ethan Lin <lyc.ethan@gmail.com>
2022-04-16 00:32:49 +0800

    Initial project

---
+++ DeepLink/DeepLink/DeepLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepLinkApp.swift
+//  DeepLink
+//
+//  Created by Ethan Lin on 2022/4/16.
+//
+
+import SwiftUI
+
+@main
+struct DeepLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
